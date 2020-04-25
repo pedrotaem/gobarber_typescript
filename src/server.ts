23ded3790c643ed9d7c978fe9e0163800fb48f38
Pedro Amaral <pedrotaem@gmail.com>
2020-04-25 20:11:34 -0300

    fix

--- src/server.ts
+++ src/server.ts
@@ -21,7 +21,6 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
       message: err.message,
     });
   }
-
   console.log(err);
 
   return response.status(500).json({
