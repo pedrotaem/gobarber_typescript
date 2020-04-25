4d7a88f4e8ed24856b095fa8d8ea87b146dbbf20
Pedro Amaral <pedrotaem@gmail.com>
2020-04-25 20:06:29 -0300

    chore: Add base back-end

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'b886776ebb4bf47c6f49192c6a83fd5c',
+    expiresIn: '1d',
+  },
+};
---
+++ src/routes/index.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+import appointmentsRouter from './appointments.routes';
+import usersRouter from './users.routes';
+import sessionsRouter from './sessions.routes';
+
+const routes = Router();
+
+routes.use('/appointments', appointmentsRouter);
+routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
+
+// routes.post('/users', (request, response) => {
+//   const { name, email } = request.body;
+//   const user = {
+//     name,
+//     email,
+//   };
+
+//   return response.json(user);
+// });
+
+export default routes;
